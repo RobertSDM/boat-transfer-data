0c3f1a0b7fabb933a7a3fff51f749c6ea1e79493
Roberto <roberto.menesantos@gmail.com>
2024-05-31 20:30:35 -0300

    feat: first commit

---
+++ src/test/java/mai_ocean/robot_data_transfer/RobotDataTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package mai_ocean.robot_data_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotDataTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
